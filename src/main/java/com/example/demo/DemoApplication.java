c0d7b0a0bab60d23a184464961997ebd7ee11b11
Audrey Neveu <aneveu@pivotal.io>
2020-08-12 10:41:23 +0200

    fix #1 Update to 2020.0, add graphs on completed and completedEmpty.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxProcessor;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.ReplayProcessor;
+import reactor.core.publisher.Sinks;
 import reactor.core.scheduler.Schedulers;
 
 @SpringBootApplication
@@ -23,42 +24,44 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	final ReplayProcessor<String> latestChange = ReplayProcessor.cacheLast();
+	final Sinks.Many<String> latestChange = Sinks.many().replay().latest();
 
 	public DemoApplication(WebClient.Builder webClientBuilder) {
-		FluxSink<String> sink = latestChange.sink(FluxSink.OverflowStrategy.LATEST);
-
 		WebClient webClient = webClientBuilder.build();
 
 		webClient.get()
 				.uri("https://stream.wikimedia.org/v2/stream/recentchange")
 				.retrieve()
 				.bodyToFlux(JsonNode.class)
-				.name("recentchange")
+				.name("recentChanges")
 				.metrics()
-				.concatMap(change -> {
-					return processChange(change)
+				.concatMap(change -> processChange(change)
+							.onErrorResume(IllegalStateException.class, __ -> Mono.empty())
+							.onErrorContinue(IllegalArgumentException.class, (t, o) -> System.out.println(t.toString()))
 							.name("processing")
-							.metrics()
-							.onErrorResume(IllegalStateException.class, __ -> Mono.empty());
-				})
-				.doOnNext(sink::next)
+							.metrics())
+				.doOnNext(latestChange::emitNext)
 				// Avoid polluting the logs
 				.sample(Duration.ofSeconds(1))
 				.log()
-				.retryWhen(it -> it.delayElements(Duration.ofSeconds(1)))
+				.retry()
 				.subscribe();
 	}
 
 	@GetMapping("/latestChange")
 	public Mono<String> latestChange() {
-		return latestChange.next();
+		return latestChange.asFlux().next();
 	}
 
 	Mono<String> processChange(JsonNode change) {
 		if (change.path("bot").asBoolean()) {
 			return Mono.error(new IllegalStateException("OMG! I don't know how to handle the bots!"));
 		}
+
+		if (!change.path("wiki").asText().contains("wiki")){
+			return Mono.error(new IllegalArgumentException("Not a wiki!"));
+		}
+
 		return Mono.just("Change to '" + change.path("title").asText(null) + "' by '" + change.path("user").asText(null) + "'");
 	}
 
