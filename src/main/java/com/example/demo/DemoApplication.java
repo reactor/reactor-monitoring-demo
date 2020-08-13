fbce8867de61f27abd34bdb79f4d64896d207a27
Audrey Neveu <aneveu@pivotal.io>
2020-08-13 18:08:01 +0200

    update code to see completed and completedEmpty whilst keeping an error to track + related graphs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxProcessor;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import reactor.core.scheduler.Schedulers;
-import reactor.util.retry.Retry;
 
 @SpringBootApplication
 @RestController
@@ -36,17 +36,16 @@ public class DemoApplication {
 				.bodyToFlux(JsonNode.class)
 				.name("recentChanges")
 				.metrics()
-				.concatMap(change -> {
-					return processChange(change)
+				.concatMap(change -> processChange(change)
+							.onErrorResume(IllegalStateException.class, __ -> Mono.empty())
+							.onErrorContinue(IllegalArgumentException.class, (t, o) -> System.out.println(t))
 							.name("processing")
-							.metrics()
-							.onErrorResume(IllegalStateException.class, __ -> Mono.empty());
-				})
+							.metrics())
 				.doOnNext(processor::onNext)
 				// Avoid polluting the logs
 				.sample(Duration.ofSeconds(1))
 				.log()
-				.retryWhen(Retry.fixedDelay(100, Duration.ofSeconds(1)))
+				.retry()
 				.subscribe();
 	}
 
@@ -55,10 +54,20 @@ public class DemoApplication {
 		return processor.next();
 	}
 
+	@GetMapping("/cancel")
+	public Flux<String> cancel(){
+		return processor.cancelOn(Schedulers.immediate());
+	}
+
 	Mono<String> processChange(JsonNode change) {
 		if (change.path("bot").asBoolean()) {
 			return Mono.error(new IllegalStateException("OMG! I don't know how to handle the bots!"));
 		}
+
+		if (!change.path("wiki").asText().contains("wiki")){
+			return Mono.error(new IllegalArgumentException("Not a wiki!"));
+		}
+
 		return Mono.just("Change to '" + change.path("title").asText(null) + "' by '" + change.path("user").asText(null) + "'");
 	}
 
